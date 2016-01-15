aabf3a58daa75211dde01860d9f1b55df5a528f5
mapineda Pineda <mapineda1222@gmail.com>
2016-01-15 15:19:17 -0600

    added styling

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ app.controller('MainCtrl', [
 		$scope.posts = [
 			{title: 'post 1', upvotes: 5},
 			{title: 'post 2', upvotes: 2},
-			{title: 'post 3', upvotes: 15},
+			{title: 'post 3', upvotes: 7},
 			{title: 'post 4', upvotes: 9},
 			{title: 'post 5', upvotes: 4}
 		];
