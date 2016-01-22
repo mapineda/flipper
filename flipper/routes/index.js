e43228e4f9976293ee972076d5bab2210a761a91
mapineda Pineda <mapineda1222@gmail.com>
2016-01-21 23:02:14 -0600

    updated get and psot routing

--- flipper/routes/index.js
+++ flipper/routes/index.js
@@ -5,5 +5,28 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+/* adding models*/
+var mongoose = require('mongoose');
+var Post = mongoose.model('Post');
+var Comment = mongoose.model('Comment');
+/* add get request */
+router.get('/posts', function(req, res, next) {
+  Post.find(function(err, posts){
+    if(err){ return next(err); }
+
+    res.json(posts);
+  });
+});
+
+router.post('/posts', function(req, res, next) {
+  var post = new Post(req.body);
+
+  post.save(function(err, post){
+    if(err){ return next(err); }
+
+    res.json(post);
+  });
+
+});
 
 module.exports = router;
