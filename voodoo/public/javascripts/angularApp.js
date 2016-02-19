d837b82770f72a77a3e498f2c45e7a86e3c41576
mapineda Pineda <mapineda1222@gmail.com>
2016-02-19 15:42:03 -0600

    update wiring server

--- voodoo/public/javascripts/angularApp.js
+++ voodoo/public/javascripts/angularApp.js
@@ -36,7 +36,14 @@ var app = angular.module('flipperNews', ['ui.router'])
 	 return $http.post('/posts', post).success(function(data){
 		 o.posts.push(data);
 	 });
-	};
+};
+
+	o.upvote = function(post) {
+  return $http.put('/posts/' + post._id + '/upvote')
+    .success(function(data){
+      post.upvotes += 1;
+    });
+};
 		return o;
 	}])
 	// main controller
